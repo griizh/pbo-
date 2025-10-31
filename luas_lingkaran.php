1c36089344e062fec893a473e345d9f75370db30
gilang ramadhan <gilang11564@gmail.com>
2025-10-31 12:58:26 +0700

    gilanng

---
+++ luas_lingkaran.php
@@ -0,0 +1,27 @@
+<?php
+
+class LuasLingkaran {
+
+    const PHI = 3.14;
+
+    public $jari;
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::PHI * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} ini hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "</br>";
+        echo "ini testing static";
+    }
+
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 10; //input nilai jari jari
+$lingkaran->tampil(); //tampilkan luas lingkaran
+
+LuasLingkaran::testing(); //memanggil method static
+//$rumus = Luaslingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+//echo "hasilnya adalah :", $rumus;git
